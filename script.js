25a1e7afab42c2f306a63c885d88ae08d4a3fdfe
BJDXYZT <zhangyy82@chinatelecom.cn>
2025-02-27 13:48:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,19 +9,14 @@ document.getElementById('calculate-button').addEventListener('click', function()
     resultsDiv.innerHTML = '<h2>计算结果:</h2>';
 
     resultsDiv.innerHTML += `
+        <hr>
         <div class="result-item"><strong>模型参数:</strong> ${param}B </div>
         <div class="result-item"><strong>精度:</strong> ${precision}</div>
         <div class="result-item"><strong>算力卡:</strong> ${getHardwareDisplayName(hardware)}</div>
-        <div class="result-item"><strong>模型权重显存需求: </strong><strong>${calculateWeightMemory(param, precision)}G</strong></div>
-        <div class="result-item"><strong>总显存需求: </strong> <strong>${calculateTotalMemory(param, precision)}G</strong></div>
-        <div class="result-item"><strong>预估算力卡需求:</strong> <strong>${calculateCompute(param, precision, hardware)}</strong></div>
-        <div class="result-item"><strong>预估算力机台数:</strong> <strong>${calculateMachineCount(param, precision, hardware)} 台</strong></div>
-        <p class="result-item" style="font-size: smaller; color: gray;">* 显存和算力均为估算值，实际情况可能因多种因素而异。</p>
-        <p class="result-item" style="font-size: smaller; color: gray;">* 请检查所选算力卡是否支持参数精度类型。</p>
-        <p class="result-item" style="font-size: smaller; color: gray;">* 推理显存估算公式：总显存需求 = 模型权重显存需求 * 1.2（经验值）</p>
-        <p class="result-item" style="font-size: smaller; color: gray;">* 算力卡数量为满足显存需求的最少估算，实际部署可能需要更多卡以满足性能需求。</p>
-        <p class="result-item" style="font-size: smaller; color: gray;">* 算力机台数假设 NVIDIA 和 华为昇腾 机器每台都包含 8 张算力卡。
-        
+        <div class="result-item"><strong>模型权重显存需求: </strong>${calculateWeightMemory(param, precision)}G</div>
+        <div class="result-item"><strong>总显存需求: </strong> ${calculateTotalMemory(param, precision)}G</div>
+        <div class="result-item"><strong>预估算力卡需求:</strong> ${calculateCompute(param, precision, hardware)} 张</div>
+        <div class="result-item"><strong>预估算力机台数:</strong> ${calculateMachineCount(param, precision, hardware)} 台</div>
     `;
 
 });
@@ -50,6 +45,7 @@ function calculateWeightMemory(param, precision) {
         'BF16': 2,
         'INT8': 1,
         'INT4': 0.5,
+        'FP8': 1,
     }
     paramSize = precisionDict[precision];
     weightMemory = param * paramSize;
@@ -80,6 +76,9 @@ function calculateCompute(param, precision, hardware) {
         'nvidia_rtx4090': 24,
     };
     compute = weightMemory / hardwareCompute[hardware];
+    console.log(weightMemory);
+    console.log(hardwareCompute[hardware]);
+    console.log(compute);
     compute = Math.ceil(compute);
     return compute;
 }
